601949dbd74ec6cf2a2cf7f6ef8288e43cae0446
tanishka telang <tanishkatelang017@gmail.com>
2023-12-10 22:32:32 +0530

    second commit

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,37 +1,53 @@
-"use client"
+"use client";
 
-import axios from 'axios';
-import { useRouter } from 'next/navigation';
-import React from 'react'
-import toast from 'react-hot-toast';
+import axios from "axios";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
+import toast from "react-hot-toast";
 
 export default function ProfilePage() {
-
   const router = useRouter();
+  const [data, setData] = useState("");
 
   const logout = async () => {
     try {
-      await axios.get('/api/users/logout');
-      toast.success('Logout Successfully');
-      router.push('/login')
+      await axios.get("/api/users/logout");
+      toast.success("Logout Successfully");
+      router.push("/login");
     } catch (error: any) {
       console.log(error.message);
       toast.error(error.message);
     }
-  }
+  };
+
+  const getUserData = async () => {
+    const res = await axios.get("/api/users/me");
+    console.log(res);
+    setData(res.data.data.username);
+  };
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <h1>Profile</h1>
       <hr />
       <p>Profile page</p>
+      <h2 className="p-1 mt-3 rounded bg-green-500 text underline">
+        {data === "" ? "" : <Link href={`/profile/${data}`}>{data}</Link>}
+      </h2>
       <button
         onClick={logout}
-        className="bg-blue-500 mt-4 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        className="bg-blue-500 mt-4 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+      >
         Logout
       </button>
 
+      <button
+        onClick={getUserData}
+        className="bg-purple-500 mt-4 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded"
+      >
+        Get User Details
+      </button>
     </div>
-
-  )
+  );
 }
--- src/middleware.ts
+++ src/middleware.ts
@@ -9,18 +9,17 @@ export function middleware(request: NextRequest) {
   
   const token = request.cookies.get('token')?.value || ''
 
-  //if user is authenticated/have token then it should not be able to access the login & signup
+   //if user is authenticated/have token then it should not be able to access the login & signup
   if(isPublicPath && token){
     return NextResponse.redirect(new URL('/', request.nextUrl))
   }
-
+  
   //if user not authenticated redirect user to login
   if(!isPublicPath && !token){
     return NextResponse.redirect(new URL('/login', request.nextUrl))
   }
 }
- 
-// See "Matching Paths" below to learn more
+
 //protected routes
 export const config = {
   matcher: [
@@ -31,3 +30,5 @@ export const config = {
     '/signup'
   ]
 }
+
+
